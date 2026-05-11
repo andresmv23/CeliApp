a39c3046c0f7b8d962d9f7919d77f127c3872a55
Andres <andresmv2310@gmail.com>
2026-05-11 19:44:39 +0200

    Cambios en el middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -53,7 +53,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
